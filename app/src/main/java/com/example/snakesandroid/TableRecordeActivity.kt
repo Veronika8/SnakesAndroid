0cc4ec58c3f5559188e6c8c56e629fffce8ffef9
veronika.bukina <veronika.bukina@mail.ru>
2020-03-12 08:46:29 +0700

    init commit

---
+++ app/src/main/java/com/example/snakesandroid/TableRecordeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.snakesandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TableRecordeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_table_recorde)
+    }
+}
